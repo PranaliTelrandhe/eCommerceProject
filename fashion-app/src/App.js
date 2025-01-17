f0a9cda5261718d59526524c566bc6f662689e32
PranaliTelrandhe <pranalitelrandhe252@gmail.com>
2025-01-17 13:52:44 +0100

    added: hook concept (program) & shopping cart code

--- fashion-app/src/App.js
+++ fashion-app/src/App.js
@@ -1,22 +1,41 @@
-import React from "react";
-import Header from "./component/Header/Header"
-import Navbar from "./component/Navigation_bar/Navbar";
-import ImageSlider from "./component/Image_slider/ImageSlider";
-import BrandPortfolio from "./component/Portfolio_brands/BrandPortfolio";
-import CategoryToBag from "./component/Category/CategoryToBag";
-import Footer from "./component/Footer/Footer";
+// Import statements should be at the top
+import React, { useReducer, createContext } from 'react'; // React and hooks
+import ShopRouter from './Router/ShopRouter'; // Local file imports
+import logo from './logo.svg'; // Asset imports
+import './App.css'; // CSS file
+
+// Create global Context Object and export
+export const ShopContext = createContext();
+
+// Reducer function for managing the shopping cart
+const cartReducer = (cart, action) => {
+  switch (action.type) {
+    case 'ADD TO CART': 
+      return [...cart, action.item];
+    case 'REMOVE': 
+      return cart.filter(cartItem => cartItem !== action.item);
+    default: 
+      return cart;
+  }
+};
 
 function App() {
+  const [cart, setCart] = useReducer(cartReducer, []);
+
+  // Handlers for shopping cart
+  const addToCart = product => {
+    setCart({ type: 'ADD TO CART', item: product });
+  };
+
+  const removeFromCart = cartItem => {
+    setCart({ type: 'REMOVE', item: cartItem });
+  };
+
   return (
     <div>
-      <Header />
-      <Navbar />
-      <ImageSlider />
-      <BrandPortfolio />
-      <CategoryToBag />
-      <Footer />
-      {/*<ProductSlider /> */}
-      {/* Other components */}
+      <ShopContext.Provider value={{ cart, addToCart, removeFromCart }}>
+        <ShopRouter />
+      </ShopContext.Provider>
     </div>
   );
 }
