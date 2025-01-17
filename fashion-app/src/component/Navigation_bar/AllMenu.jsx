46692ea0c68c85b4ac680a63e30e3ff095f76144
PranaliTelrandhe <pranalitelrandhe252@gmail.com>
2025-01-17 13:56:32 +0100

    import : Link from react router dom

--- fashion-app/src/component/Navigation_bar/AllMenu.jsx
+++ fashion-app/src/component/Navigation_bar/AllMenu.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./AllMenu.css";
+import { Link } from 'react-router-dom'; 
 
 const AllMenu = () => {
   const [showMenu, setShowMenu] = useState(false);
@@ -18,11 +19,11 @@ const AllMenu = () => {
       {/* Dropdown Menu */}
       {showMenu && (
         <div className="allMenu__dropdown">
-          <a href="#" className="allMenu__item">Men</a>
-          <a href="#" className="allMenu__item">Women</a>
-          <a href="#" className="allMenu__item">Kids</a>
-          <a href="#" className="allMenu__item">Home & Living</a>
-          <a href="#" className="allMenu__item">Beauty & Personal Care</a>
+          <Link to="/men" className="allMenu__item">Men</Link>
+          <Link to="/women" className="allMenu__item">Women</Link>
+          <Link to="/kids" className="allMenu__item">Kids</Link>
+          <Link to="/home" className="allMenu__item">Home & Living</Link>
+          <Link to="/beauty" className="allMenu__item">Beauty & Personal Care</Link>
         </div>
       )}
     </div>
