ba79f9d880c500444d67ab42b46c5970aa7da498
PranaliTelrandhe <pranalitelrandhe252@gmail.com>
2025-01-17 13:58:04 +0100

    import: Link & outlet from react-router-dom

--- fashion-app/src/component/Navigation_bar/Navbar.jsx
+++ fashion-app/src/component/Navigation_bar/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./Navbar.css";
 import AllMenu from "./AllMenu";
+import {Link,Outlet} from 'react-router-dom';
+
 
 const Navbar = () => {
+
+  
   return (
+    <>
     <nav className="navbar">
       <div className="navbar__container">
         <div className="navbar__menu">
@@ -11,14 +16,15 @@ const Navbar = () => {
           <AllMenu />
         </div>
         <div className="navbar__links">
-          <a href="#" className="navbar__link">Today's Deals</a>
-          <a href="#" className="navbar__link">Customer Service</a>
-          <a href="#" className="navbar__link">Registry</a>
-          <a href="#" className="navbar__link">Gift Cards</a>
-          <a href="#" className="navbar__link">Sell</a>
+          <Link to="/deal" className="navbar__link">Today's Deals</Link>
+          <Link to="/customer" className="navbar__link">Customer Service</Link>
+          <Link to="/resigstry" className="navbar__link">Registry</Link>
+          <Link to="/gift" className="navbar__link">Gift Cards</Link>
+          <Link to="/sell" className="navbar__link">Sell</Link>
         </div>
       </div>
     </nav>
+    <Outlet /></>
   );
 };
 
