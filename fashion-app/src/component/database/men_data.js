bc3fd2be792d341204ef3d64a29a109bb0ad7c56
PranaliTelrandhe <pranalitelrandhe252@gmail.com>
2025-01-22 12:44:17 +0100

    Changes database: Multiple collection added

--- fashion-app/src/component/database/men_data.js
+++ fashion-app/src/component/database/men_data.js
@@ -1,35 +1,22 @@
-const men = [
-    {
-      model: "Nexon",
-      brand: "Tata",
-      price: 1200.0,
-      image: "/assets/images/men1.jpg",
+const menProducts = Array.from({ length: 100 }, (v, j) => {
+  const price = Math.floor(999 + Math.random() * 3000); // Random price between 999 and 3999
+  const discount = [10, 15, 20, 25, 30][j % 5]; // Random discount percentage from a fixed set
+  const discountedPrice = Math.floor(price - (price * discount) / 100); // Apply discount
+
+  return {
+    id: j + 1,
+    name: `Prod ${j + 1}`,
+    brand: ["H&M", "Levi's", "Nike", "Adidas", "Zara", "Puma", "Biba", "FabIndia"][j % 8],
+    price,
+    discount,
+    discountedPrice, // Add discounted price
+    specification: {
+      size: ["S, M, L, XL", "M, L, XL, XXL"][j % 2],
+      material: ["Cotton", "Polyester", "Denim", "Viscose"][j % 4],
+      color: ["Red", "Blue", "Black", "White", "Green"][j % 5],
     },
-    {
-      model: "Wagon R",
-      brand: "Maruti Suzuki",
-      price: 1500000.0,
-      image: "/assets/images/men1.jpg",
-    },
-    {
-      model: "Verna",
-      brand: "Hyundai",
-      price: 1500000.0,
-      image: "/assets/images/men1.jpg",
-    },
-    {
-      model: "Safari",
-      brand: "Tata",
-      price: 1500000.0,
-      image: "/assets/images/men1.jpg",
-    },
-    {
-      model: "Carens",
-      brand: "Kia",
-      price: 1900000.0,
-      image: "/assets/images/men1.jpg",
-    },
-  ];
-  
-  export default men;
-  
+    image: `/assets/images/women/prod${j + 1}.jpg`,
+  };
+});
+
+export default menProducts;
