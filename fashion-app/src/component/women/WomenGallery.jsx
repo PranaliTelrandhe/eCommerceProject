bb8de8651a69277bfec8314c5c8b3d51ef4b1cc9
PranaliTelrandhe <pranalitelrandhe252@gmail.com>
2025-01-30 13:28:27 +0100

    Added womenGallery file

--- fashion-app/src/component/women/WomenGallery.jsx
+++ fashion-app/src/component/women/WomenGallery.jsx
@@ -1,14 +1,40 @@
-import React from 'react';
-import WomenCard from './WomenCard';
-import womenProducts from './women_data';
+import React, { useState } from "react"; //  useState
+import WomenCard from "./WomenCard";
+import women from "./women_data";
+import { filterWomenByBrand, filterUniqueBrands } from "../women/Brand_wfilter";
+import BrandWbutton from "../women/BrandWbutton";
 
 const WomenGallery = () => {
+  const [filteredResult, setFilteredResult] = useState(women);
+
+  const uniqueBrands = filterUniqueBrands(women);
+  uniqueBrands.unshift("All");
+
   return (
     <div className="container my-4">
       <h2 className="text-center mb-4">Women Collection</h2>
+      
+      <div className="d-flex flex-column gap-4 my-4">
+        {uniqueBrands.map((brand) => (
+          <BrandWbutton
+            key={brand}
+            brandStyle={
+              brand === "All" ? "btn btn-outline-primary" : "btn btn-outline-secondary"
+            }
+            onFilter={
+              brand === "All"
+                ? () => setFilteredResult(women)
+                : () => setFilteredResult(filterWomenByBrand(women, brand))
+            }
+          >
+            {brand}
+          </BrandWbutton>
+        ))}
+      </div>
+
       {/* Grid layout with responsive breakpoints */}
       <div className="row">
-        {womenProducts.map((product) => (
+        {filteredResult.map((product) => ( //Fixed from womenProducts to filteredResult
           <div className="col-6 col-sm-4 col-md-3 mb-4" key={product.id}>
             <WomenCard product={product} />
           </div>
